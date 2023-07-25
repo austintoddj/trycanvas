3f22d5ca730ced608cf15ec62faa87e95f9b9876
Todd Austin <austin.todd.j@gmail.com>
2023-07-25 16:22:22 -0500

    one more try

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [vue()],
+    define: {
+        'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(
+            process.env.VERCEL_ANALYTICS_ID
+        )
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
