1b8a02badb8fd06dd22b8ad4f3ede79dd76d09b5
Todd Austin <austin.todd.j@gmail.com>
2023-07-25 11:12:42 -0500

    adding support for speed insights

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import './style.css'
 import App from './App.vue'
 import VueGtag from 'vue-gtag'
 import { inject } from '@vercel/analytics'
+import reportWebVitals from './reportWebVitals'
+import { sendToVercelAnalytics } from './vitals'
 
 inject()
 
+reportWebVitals(sendToVercelAnalytics)
+
 createApp(App)
     .use(VueGtag, {
-        config: { id: 'G-3PS6P0GB6J' }
+        config: { id: import.meta.env.VITE_GA_MEASUREMENT_ID }
     })
     .mount('#app')
