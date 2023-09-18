22a38d8f8f8925bb1135ea0fabb753e7838fb6db
Todd Austin <austin.todd.j@gmail.com>
2023-09-18 15:30:41 -0500

    linting and chores

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,4 @@
-import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals'
+import { onCLS, onFCP, onFID, onLCP, onTTFB } from 'web-vitals'
 
 const reportWebVitals = onPerfEntry => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
@@ -9,7 +9,7 @@ const reportWebVitals = onPerfEntry => {
             onLCP(onPerfEntry)
             onTTFB(onPerfEntry)
         } catch (err) {
-            console.error('[Analytics]', err)
+            throw new Error(err)
         }
     }
 }
