8a5536bb8cfa63d923881985c41a0ade00b7d438
Todd Austin <austin.todd.j@gmail.com>
2023-12-29 12:41:33 -0600

    is vue-router required by speed-insights?

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import App from './App.vue'
-import reportWebVitals from './reportWebVitals'
 import './style.css'
-import { sendToVercelAnalytics } from './vitals'
 import { inject } from '@vercel/analytics'
 import { createApp } from 'vue'
 
 inject()
-
-reportWebVitals(sendToVercelAnalytics)
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(
-      process.env.VERCEL_ANALYTICS_ID
-    )
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
