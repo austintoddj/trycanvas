56c1817ec1731d74e34ea5b92e056ec897b2bb04
Todd Austin <austin.todd.j@gmail.com>
2025-03-30 11:43:53 -0500

    updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,19 @@
-import prettierConfig from 'eslint-config-prettier'
-import vue from 'eslint-plugin-vue'
-import vueParser from 'vue-eslint-parser'
+import prettier from 'eslint-config-prettier'
+import pluginVue from 'eslint-plugin-vue'
 
 export default [
   {
     ignores: ['node_modules', 'dist']
   },
+  ...pluginVue.configs['flat/recommended'],
   {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parser: vueParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-    },
-    plugins: {
-      vue
-    },
     rules: {
-      ...prettierConfig.rules,
-      ...vue.configs['vue3-recommended'].rules,
-      'no-unused-vars': 'error',
-      'no-console': 'error'
-    }
-  },
-  {
-    files: ['**/*.js'],
+      ...prettier.rules,
+      'vue/no-unused-vars': 'error'
+    },
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module'
-    },
-    rules: {
-      'no-unused-vars': 'error',
-      'no-console': 'error'
     }
   }
 ]
